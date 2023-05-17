69f07eaa5a36b66bf9288fa0b6a850b89a71d91d
umerfarooqnxb <umer.farooq2885@nxb.com.pk>
2023-05-17 19:20:15 +0500

    S3 Bucket implementation

--- server.js
+++ server.js
@@ -1,11 +1,32 @@
 const express = require('express');
+const { upload } = require('./src/utils/s3bucket');
+
+const images = upload().single('image')
 
 // dotenv load the .env variables into process.env
 require('dotenv').config()
 
+// multer 
+// const multer  = require('multer')
+// const upload = multer({ dest: 'uploads/' })
+
 // Instance of express app
 const app = express()
 
+// routes to post
 
+app.post('/upload-media', function (req, res, next) {
+    console.log(req)
+    images(req, res, (err) => {
+        if (err) {
+            res.send(err)
+        }
+        next()
+    })
+},
+    (req, res) => {
+        const file = req?.file
+        res.send({ file })
+    })
 
 app.listen(process.env.PORT, () => console.log(`App is listening on ${process.env.PORT} post`))
---
+++ src/utils/s3bucket.js
@@ -0,0 +1,32 @@
+const S3 = require('aws-sdk/clients/s3')
+const multer = require('multer')
+const multerS3 = require('multer-s3')
+require('dotenv').config()
+
+const s3 = new S3({
+    region: process.env.AWS_REGION,
+    accessKeyId: process.env.AWS_ACCESS_KEY,
+    secretAccessKey: process.env.AWS_SECRET_KEY
+})
+
+
+const upload = () =>
+    multer({
+        storage: multerS3({
+            s3: s3,
+            bucket: `my-media-uploader`,
+            metadata: (req, file, cb) => {
+                cb(null, { fieldName: file.fieldname })
+            },
+            key: (req, file, cb) => {
+                cb(null, Date.now().toString)
+            }
+        })
+    })
+
+
+module.exports = {
+    s3,
+    upload
+}
+
